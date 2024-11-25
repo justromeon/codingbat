ff8b0b3dcf04dd4c5c5631d9eeec87673086987a
Justin Kian Romeon <romeonjustin@gmail.com>
2024-11-25 21:25:51 +0800

    Make Spec.hs as entry point, separate hs files for unit tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,17 +1,8 @@
 module Main (main) where
 
 import Test.Hspec
-import Warmup1 (sleepIn)
+import qualified Warmup1Spec
 
 main :: IO ()
 main = hspec $ do
-  describe "Warmup1.sleepIn" $ do
-    it "allows sleeping in on weekdays if not a weekday and not a vacation" $
-      sleepIn False False `shouldBe` True
-
-    it "does not allow sleeping in on weekdays if it is a weekday and not a vacation" $
-      sleepIn True False `shouldBe` False
-
-    it "allows sleeping in if it is a vacation, regardless of weekday" $ do
-      sleepIn False True `shouldBe` True
-      sleepIn True True `shouldBe` True
+  describe "Warmup1 module tests" Warmup1Spec.spec
---
+++ test/Warmup1Spec.hs
@@ -0,0 +1,16 @@
+module Warmup1Spec (spec) where
+
+import Test.Hspec
+import Warmup1 (sleepIn)
+
+spec :: Spec
+spec = describe "Warmup1.sleepIn" $ do
+  it "allows sleeping in on weekdays if not a weekday and not a vacation" $
+    sleepIn False False `shouldBe` True
+
+  it "does not allow sleeping in on weekdays if it is a weekday and not a vacation" $
+    sleepIn True False `shouldBe` False
+
+  it "allows sleeping in if it is a vacation, regardless of weekday" $ do
+    sleepIn False True `shouldBe` True
+    sleepIn True True `shouldBe` True
