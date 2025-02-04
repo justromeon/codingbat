4906936d5d061024dbe06e361bcaad98350b1883
Justin Kian Romeon <romeonjustin@gmail.com>
2025-02-04 23:19:01 +0800

    Create Warmup2 tests file and add stringTimes function test cases

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,7 +2,9 @@ module Main (main) where
 
 import Test.Hspec
 import qualified Warmup1Spec
+import qualified Warmup2Spec
 
 main :: IO ()
 main = hspec $ do
   describe "Warmup1 module tests" Warmup1Spec.spec
+  describe "Warmup2 module tests" Warmup2Spec.spec
