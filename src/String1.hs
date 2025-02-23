24f4f72f1cbf623dea1a0af542168a2cddffa7a8
Justin Kian Romeon <romeonjustin@gmail.com>
2025-02-23 08:31:07 +0800

    Refactor String1: generalize some functions from String to [a]

--- src/String1.hs
+++ src/String1.hs
@@ -4,16 +4,16 @@ import Data.Function (on)
 helloName :: String -> String
 helloName name = "Hello " ++ name ++ "!" 
 
-makeOutWord :: String -> String -> String
+makeOutWord :: [a] -> [a] -> [a]
 makeOutWord out word = left ++ word ++ right
   where
     (left,right) = splitAt (length out - 2) out
 
-firstHalf :: String -> String
+firstHalf :: [a] -> [a]
 firstHalf = take =<< (`div` 2) . length
 
-nonStart :: String -> String -> String
+nonStart :: [a] -> [a] -> [a]
 nonStart = (++) `on` drop 1
 
-makeAbba :: String -> String -> String
+makeAbba :: [a] -> [a] -> [a]
 makeAbba xs ys = concat [xs,ys,ys,xs]
