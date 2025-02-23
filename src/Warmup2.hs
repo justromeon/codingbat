2e30a3835a65db869fb8bb04a439d142b12c7961
Justin Kian Romeon <romeonjustin@gmail.com>
2025-02-23 08:29:15 +0800

    Refactor Warmup2: generalize some functions from String to [a]

--- src/Warmup2.hs
+++ src/Warmup2.hs
@@ -3,19 +3,19 @@ module Warmup2 where
 import           Data.Function (on)
 import qualified Data.Set as S
 
-stringTimes :: String -> Int -> String
-stringTimes str n = concat (replicate n str)
+stringTimes :: [a] -> Int -> [a]
+stringTimes xs n = concat (replicate n xs)
 
-stringSplosion :: String -> String
+stringSplosion :: [a] -> [a]
 stringSplosion = concat . foldr (\x acc -> [x] : map (x:) acc) []
 
 arrayFront9 :: [Int] -> Bool
 arrayFront9 = elem 9 . take 4
 
-frontTimes :: String -> Int -> String
+frontTimes :: [a] -> Int -> [a]
 frontTimes str = concat . flip replicate (take 3 str)
 
-last2 :: String -> Int
+last2 :: Eq a => [a] -> Int
 last2 str = length $ filter (\(l2,l1) -> [l2,l1] == final2) $ zip rest (tail rest)
   where
     (rest, final2) = splitAt (length str - 2) str
@@ -23,7 +23,7 @@ last2 str = length $ filter (\(l2,l1) -> [l2,l1] == final2) $ zip rest (tail res
 array123 :: [Int] -> Bool
 array123 = elem (1,2,3) . (zip3 <$> id <*> drop 1 <*> drop 2)
 
-stringBits :: String -> String
+stringBits :: [a] -> [a]
 stringBits []  = []
 stringBits [x] = [x]
 stringBits (x:_:zs) = x : stringBits zs
@@ -31,7 +31,7 @@ stringBits (x:_:zs) = x : stringBits zs
 arrayCount9 :: [Int] -> Int
 arrayCount9 = length . filter (== 9)
 
-stringMatch :: String -> String -> Int
+stringMatch :: Ord a => [a] -> [a] -> Int
 stringMatch = (S.size .) . (S.intersection `on` S.fromList . bigrams)
   where
     bigrams = zip <*> tail
