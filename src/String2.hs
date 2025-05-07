394693dc141ae3bb956fe853ac3b40b1a494e149
Justin Kian Romeon <romeonjustin@gmail.com>
2025-05-07 22:53:03 +0800

    Add String2.countCode

--- src/String2.hs
+++ src/String2.hs
@@ -2,3 +2,8 @@ module String2 where
 
 doubleChar :: String -> String
 doubleChar = (<* [1..2])
+
+countCode :: String -> Int
+countCode ('c':'o':_:'e':rest) = 1 + countCode rest
+countCode []     = 0
+countCode (_:xs) = countCode xs
