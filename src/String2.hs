0c4ae861de85d15b25c5ac3f054339305e880276
Justin Kian Romeon <romeonjustin@gmail.com>
2025-05-09 18:45:22 +0800

    Add String2.endOther

--- src/String2.hs
+++ src/String2.hs
@@ -1,4 +1,7 @@
 module String2 where
+import Data.Char (toLower)
+import Data.Function (on)
+import Data.List (isSuffixOf)
 
 doubleChar :: String -> String
 doubleChar = (<* [1..2])
@@ -12,3 +15,6 @@ countHi :: String -> Int
 countHi = length . filter (== ('h','i')) . bigrams
   where
     bigrams = zip <*> tail
+
+endOther :: String -> String -> Bool
+endOther = (\s1 s2 -> s1 `isSuffixOf` s2 || s2 `isSuffixOf` s1) `on` map toLower
