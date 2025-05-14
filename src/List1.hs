214921b4a54b412cb3542fd0aa08c5289cf396c4
Justin Kian Romeon <romeonjustin@gmail.com>
2025-05-14 20:33:50 +0800

    Add List1.commonEnd

--- src/List1.hs
+++ src/List1.hs
@@ -3,3 +3,7 @@ import Data.Function (on)
 
 firstLast6 :: [Int] -> Bool
 firstLast6 = ( (||) `on` (==6) ) <$> head <*> last
+
+commonEnd :: [Int] -> [Int] -> Bool
+commonEnd [x] [y]       = x == y
+commonEnd (x:xs) (y:ys) = x == y || last xs == last ys
