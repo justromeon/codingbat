b6b8849a966e8a38b25f1943eced64bb4765596e
Justin Kian Romeon <romeonjustin@gmail.com>
2025-05-15 23:09:40 +0800

    Add List1.reverse3

--- src/List1.hs
+++ src/List1.hs
@@ -7,3 +7,6 @@ firstLast6 = ( (||) `on` (==6) ) <$> head <*> last
 commonEnd :: [Int] -> [Int] -> Bool
 commonEnd [x] [y]       = x == y
 commonEnd (x:xs) (y:ys) = x == y || last xs == last ys
+
+reverse3 :: [a] -> [a]
+reverse3 = reverse
