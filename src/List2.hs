96d2637926bff8d159e85dec7997ba8608432698
Justin Kian Romeon <romeonjustin@gmail.com>
2025-06-10 22:19:09 +0800

    Add List2.sum13

--- src/List2.hs
+++ src/List2.hs
@@ -2,3 +2,9 @@ module List2 where
 
 countEvens :: [Int] -> Int
 countEvens = length . filter even
+
+sum13 :: [Int] -> Int
+sum13 [] = 0
+sum13 xs = sum before13 + sum13 (drop 2 rest)
+  where
+    (before13,rest) = break (==13) xs
