4734b04246adb7590d1f160058ad16da1fcd85c8
Justin Kian Romeon <romeonjustin@gmail.com>
2025-06-12 22:06:18 +0800

    add List2.sum67

--- src/List2.hs
+++ src/List2.hs
@@ -11,3 +11,8 @@ sum13 xs = sum before13 + sum13 (drop 2 rest)
 
 bigDiff :: [Int] -> Int
 bigDiff = abs . (subtract <$> minimum <*> maximum)
+
+sum67 :: [Int] -> Int
+sum67 [] = 0
+sum67 (6:xs) = sum67 . drop 1 . dropWhile (/=7) $ xs
+sum67 (x:xs) = x + sum67 xs
